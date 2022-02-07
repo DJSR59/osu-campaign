75795631c5e5791e6ed331aeda18927fbcbbcfeb
djs5482 <djs5482@g.rit.edu>
2022-02-07 00:49:23 -0500

    Added a delete menu

--- campaign_gui/src/Beatmap.js
+++ campaign_gui/src/Beatmap.js
@@ -1,11 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import {Row, Col} from 'reactstrap';
+import {Row, Col, Modal, ModalBody, ModalHeader} from 'reactstrap';
 
 export default class BeatmapInfo extends React.Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      isDeleteMenuOpen: false,
+    }
+  }
+
+  displayDeleteBeatmapMenu()
+  {
+    return(
+      <Modal
+        isOpen={this.state.isDeleteMenuOpen}
+      >
+        <ModalHeader>
+          Are you sure you want to delete this beatmap?
+        </ModalHeader>
+        <ModalBody>
+          <button
+            onClick={function noRefCheck(){}}
+            children="Delete"
+          />
+          <button
+            onClick={() => this.toggleDeleteBeatmapMenu()}
+            children="Cancel"
+          />
+        </ModalBody>
+      </Modal>
+    );
+  }
+
+  toggleDeleteBeatmapMenu()
+  {
+    if (this.state.isDeleteMenuOpen === false)
+      this.setState({isDeleteMenuOpen: true});
+    else
+      this.setState({isDeleteMenuOpen: false});
+  }
+
   render()
   {
+    let deleteMenu = this.displayDeleteBeatmapMenu();
+
     return (
       <div>
           <Row className="d-flex align-items-center justify-content-center">
@@ -26,6 +67,14 @@ export default class BeatmapInfo extends React.Component
             </Col>
             <Col>
               {this.props.mapLink}
+            </Col>
+            <Col>
+              <button
+                className="button"
+                onClick={() => this.toggleDeleteBeatmapMenu()}
+                children="Delete"
+              />
+              {deleteMenu}
             </Col>
           </Row>
       </div>
